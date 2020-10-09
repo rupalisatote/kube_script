8d4d2f2d0f0a0f236d97deeb4e0df8e1446aef55
rupalisatote <31959486+rupalisatote@users.noreply.github.com>
2020-10-09 12:52:33 +0530

    Update kube-master.sh

--- kube-master.sh
+++ kube-master.sh
@@ -27,8 +27,8 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 # add kubernetes dependency
 echo "[TASK 3] Add kubernetes dependency"
-cat /etc/apt/sources.list.d/kubernetes.list<<EOF
-deb http://apt.kubernetes.io/ kubernetes-xenial main
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+> deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 sudo apt-get update -y
